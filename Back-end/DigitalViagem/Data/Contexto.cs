3754c97627a8b3852cd24296562d1ba69fe1935d
David Vasconcellos <davidvasconcellos16@gmail.com>
2021-12-16 18:55:18 -0300

    Adição da conexão com o banco e criação da tabela

--- Back-end/DigitalViagem/Data/Contexto.cs
+++ Back-end/DigitalViagem/Data/Contexto.cs
@@ -8,12 +8,13 @@ namespace DigitalViagem.Data
         public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=David; Initial Catalog=CRUDVIAGEM; Integrated Security=True");
+            optionsBuilder.UseSqlServer("Data Source=DAVID; Initial Catalog=CRUDVIAGEM; Integrated Security=True");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(table => { 
+            modelBuilder.Entity<Cliente>(table => 
+            { 
                 table.ToTable("Clientes");
                 table.HasKey(prop => prop.Id);
                 table.Property(prop => prop.Nome).HasMaxLength(40).IsRequired();
@@ -21,7 +22,7 @@ namespace DigitalViagem.Data
                 table.Property(prop => prop.Data_Nascimento).HasColumnType("date");
                 table.Property(prop => prop.Telefone).HasColumnType("char(15)").IsRequired();
                 table.Property(prop => prop.Endereco).HasColumnType("char(50)").IsRequired();
-                table.Property(prop => prop.Situacao).HasConversion<string>(); 
+                table.Property(prop => prop.Situacao).HasConversion<string>().HasMaxLength(9); 
             });
         }
     }
