fad14a0763a2e4e01c4120400731c13d57c63872
David Vasconcellos <davidvasconcellos16@gmail.com>
2021-12-16 19:50:38 -0300

    Testando o banco de dados e o método GET

--- Back-end/DigitalViagem/Controllers/ClienteController.cs
+++ Back-end/DigitalViagem/Controllers/ClienteController.cs
@@ -2,6 +2,8 @@
 using DigitalViagem.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System;
+using DigitalViagem.Enum;
 
 namespace DigitalViagem.Controllers
 {
@@ -13,6 +15,19 @@ namespace DigitalViagem.Controllers
         public IActionResult Index()
         {
             var dbcontext = new Contexto();
+            //Teste
+            var cliente = new Cliente();
+
+            cliente.Nome = "David";
+            cliente.CPF = "00000000000";
+            cliente.Data_Nascimento = DateTime.Now;
+            cliente.Endereco = "Rua nossa senhora da luz, n 8, Salvador, BA";
+            cliente.Telefone = "71999999999";
+            cliente.Situacao = Situacao.Ativo;
+            dbcontext.Add(cliente);
+            dbcontext.SaveChanges();
+            //Fim do Teste
+
             ViewData["clientes"] = dbcontext.Clientes.Where(p => p.Id>0).ToList();
             return View();
         }
