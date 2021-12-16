358459ee398899f5ada1e3ddb9c5eebfde2b335f
David Vasconcellos <davidvasconcellos16@gmail.com>
2021-12-16 19:28:02 -0300

    Adição da tabela para o método get

---
+++ Back-end/DigitalViagem/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using DigitalViagem.Models;
+using DigitalViagem.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DigitalViagem.Controllers
+{
+    [Route("Cliente")]
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public IActionResult Index()
+        {
+            var dbcontext = new Contexto();
+            ViewData["clientes"] = dbcontext.Clientes.Where(p => p.Id>0).ToList();
+            return View();
+        }
+    }
+}
