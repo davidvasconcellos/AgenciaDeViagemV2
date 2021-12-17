66c84428bf13409a8fc7e059070b1ba03711f051
David Vasconcellos <davidvasconcellos16@gmail.com>
2021-12-16 21:14:35 -0300

    Adição dos métodos GET e POST

--- Back-end/DigitalViagem/Controllers/ClienteController.cs
+++ Back-end/DigitalViagem/Controllers/ClienteController.cs
@@ -10,26 +10,26 @@ namespace DigitalViagem.Controllers
     [Route("Cliente")]
     public class ClienteController : Controller
     {
+        //Método GET
         [HttpGet]
         [Route("Listar")]
         public IActionResult Index()
         {
             var dbcontext = new Contexto();
-            //Teste
-            var cliente = new Cliente();
-
-            cliente.Nome = "David";
-            cliente.CPF = "00000000000";
-            cliente.Data_Nascimento = DateTime.Now;
-            cliente.Endereco = "Rua nossa senhora da luz, n 8, Salvador, BA";
-            cliente.Telefone = "71999999999";
-            cliente.Situacao = Situacao.Ativo;
-            dbcontext.Add(cliente);
-            dbcontext.SaveChanges();
-            //Fim do Teste
 
             ViewData["clientes"] = dbcontext.Clientes.Where(p => p.Id>0).ToList();
             return View();
+        }
+
+        //Método POST
+        [HttpPost]
+        [Route("Incluir")]
+        public IActionResult Incluir(Cliente cliente)
+        {
+            var dbcontext = new Contexto();
+            dbcontext.Add(cliente);
+            dbcontext.SaveChanges();
+            return RedirectToAction("Index");   
         }
     }
 }
