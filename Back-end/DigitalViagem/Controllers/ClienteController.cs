aaa73ec6d9948d3240d034f1029ab51665626c2e
David Vasconcellos <davidvasconcellos16@gmail.com>
2021-12-18 19:30:23 -0300

    Adição dos métodos UPDATE e DELETE

--- Back-end/DigitalViagem/Controllers/ClienteController.cs
+++ Back-end/DigitalViagem/Controllers/ClienteController.cs
@@ -31,5 +31,37 @@ namespace DigitalViagem.Controllers
             dbcontext.SaveChanges();
             return RedirectToAction("Index");   
         }
+        [HttpPost]
+        [Route("Deletar")]
+        public IActionResult Deletar(Cliente cliente)
+        {
+            var dbcontext = new Contexto();
+
+            var clienteDelete = dbcontext.Clientes.Find(cliente.Id);
+            dbcontext.Remove(clienteDelete);
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("Atualizar")]
+        public IActionResult Atualizar(Cliente novosDadosCliente)
+        {
+            var dbcontext = new Contexto();
+
+            var antigosDadosCliente = dbcontext.Clientes.Find(novosDadosCliente.Id);
+
+            antigosDadosCliente.Nome = novosDadosCliente.Nome;
+            antigosDadosCliente.CPF = novosDadosCliente.CPF;
+            antigosDadosCliente.Telefone = novosDadosCliente.Telefone;
+            antigosDadosCliente.Endereco = novosDadosCliente.Endereco;
+            antigosDadosCliente.Data_Nascimento = novosDadosCliente.Data_Nascimento;
+            antigosDadosCliente.Situacao = novosDadosCliente.Situacao;
+
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
